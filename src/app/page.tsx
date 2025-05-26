a6efc8b56b79af62b1247d47b9767f18d05fcd8a
aadrikasingh18 <aadrikas16@gmail.com>
2025-05-26 15:57:03 +0530

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,33 @@
-// src/app/page.tsx
 import { headers } from 'next/headers';
 import { UAParser } from 'ua-parser-js';
+import DeviceDetector from 'device-detector-js';
 
-export default async function HomePage() {
-  const headersList = await headers(); // ✅ await here
+export default async function HomePage() 
+{
+  const headersList = await headers(); 
   const userAgent = headersList.get('user-agent') || '';
-
-  console.log("User agent >>> ", userAgent);
-
   const parser = new UAParser(); // Create a new instance of UAParser
   parser.setUA(userAgent); // Set the user agent string to parse
 
+const deviceDetector = new DeviceDetector();
 
+  const devicesys = deviceDetector.parse(userAgent);
   const result = parser.getResult(); 
+  
+  console.log("Device >>> ", devicesys);
+  console.log("Result >>> ", result);
 
-  console.log(result);
-
-  console.log('[VISITOR]');
-  console.log(`OS: ${result.os.name} ${result.os.version}`);
-  console.log(`Browser: ${result.browser.name} ${result.browser.version}`);
-  console.log(`Device: ${result.device.vendor ?? 'Unknown'} ${result.device.model ?? 'Unknown'}`);
+  console.log(`OS Name: ${result.os.name}`)
+  console.log(`OS Version: ${result.os.version}`);
+  console.log(`Browser Name: ${result.browser.name}`);
+  console.log(`Browser Version: ${result.browser.version}`);
+  console.log(`Device Type: ${devicesys.device?.type}`);
+  console.log(`Device Brand: ${devicesys.device?.brand}`);
+  console.log(`Device Model: ${devicesys.device?.model}`);
 
   return (
     <main>
       <h1>Welcome to the Device Detection Demo</h1>
-      <p>Your device has been logged on the server 👀</p>
     </main>
   );
 }
